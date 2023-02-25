73189718b2c12cde0c94cdd9a8791897d2d05458
Andre Fong <fongandre123@gmail.com>
2023-02-25 03:03:41 -0500

    fix: Attempt Vercel fix #1

--- components/Board.tsx
+++ components/Board.tsx
@@ -39,16 +39,14 @@ export default function Board({
   // Load legal words (dictionary)
   const [words, setWords] = useState<string[]>([]);
   console.log(`words: ${words.length}`);
-  console.log(`LIVE_PRODUCTION: ${process.env.LIVE_PRODUCTION}`);
-  console.log(`VERCEL: ${process.env.VERCEL}`);
-  console.log(`VERCEL_URL: ${process.env.VERCEL_URL}`);
-  console.log(`NEXT_PUBLIC_VERCEL_URL: ${process.env.NEXT_PUBLIC_VERCEL_URL}`);
+  console.log(`NEXT_PUBLIC_VERCEL: ${process.env.NEXT_PUBLIC_VERCEL}`);
 
   useEffect(() => {
     fetch("/words.txt")
       .then((res) => res.text())
       .then((text) => {
-        if (process.env.VERCEL_URL) setWords(text.split("\n").slice(0, -1));
+        if (process.env.NEXT_PUBLIC_VERCEL_URL)
+          setWords(text.split("\n").slice(0, -1));
         else setWords(text.split("\r\n").slice(0, -1));
       });
   }, []);
