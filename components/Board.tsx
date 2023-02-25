e486c8490c8bb056131681944d6fc188d4a7c018
Andre Fong <fongandre123@gmail.com>
2023-02-25 02:04:05 -0500

    chore: Increase animation wait time for consistency

--- components/Board.tsx
+++ components/Board.tsx
@@ -59,7 +59,7 @@ export default function Board({
           setCurrentRowStatus("building");
           resetGuess();
           enableInput();
-        }, 1800);
+        }, 1900);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
