a2b04b0cf44645fc9dd3ce92befec6538aed3b08
Andre Fong <fongandre123@gmail.com>
2023-03-10 22:42:47 -0500

    feat: Display stage numbers and style game length

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,79 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import styles from "@/styles/Header.module.scss";
 import { useRouter } from "next/router";
 import useScreenSize from "@/utils/useScreenSize";
+import { useAnswer } from "@/components/AnswerContext";
+import Link from "next/link";
+import { gameConfig } from "@/utils/gameConfig";
+
+type GameLength = "short" | "medium" | "long";
 
 export default function Header() {
   const router = useRouter();
   const gameLength = router.pathname.slice(1) || "short";
+  const isValidGameLength = ["short", "medium", "long"].includes(gameLength);
 
   const { width } = useScreenSize();
+  const { answer } = useAnswer();
+
+  const [currentStage, setCurrentStage] = useState<number>(1);
+  const [totalStages, setTotalStages] = useState<number>(1);
+
+  // Calculate current stage and total stages
+  useEffect(() => {
+    if (["short", "medium", "long"].includes(gameLength) && answer) {
+      const levels = Object.keys(gameConfig[gameLength as GameLength]).map(
+        (key) => parseInt(key)
+      );
+      const answerLength = answer.length;
+      const currentStage =
+        levels.findIndex((level) => level === answerLength) + 1;
+
+      setTotalStages(levels.length);
+      setCurrentStage(currentStage);
+    }
+
+    return () => {
+      setTotalStages(1);
+      setCurrentStage(1);
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [answer]);
 
   function handleNextGameLength() {
     if (gameLength === "medium") router.push("/long");
     else if (gameLength === "long") router.push("/");
-    else router.push("/medium");
+    else if (gameLength === "short") router.push("/medium");
+    else router.push("/");
+  }
+
+  function handleGameLengthColor(gameLength: string) {
+    let className = `${styles.length} `;
+    if (!["short", "medium", "long"].includes(gameLength)) return className;
+    else if (gameLength === "short") className += styles.green;
+    else if (gameLength === "medium") className += styles.yellow;
+    else if (gameLength === "long") className += styles.red;
+    return className;
   }
 
   return (
     <header className={styles.content}>
-      {width && width <= 300 ? (
+      {!isValidGameLength ? (
+        <div className={styles.title_container}>
+          <Link href="/">
+            <h1 className={styles.title}>Wordle Step</h1>
+          </Link>
+        </div>
+      ) : width && width <= 300 ? (
         <>
           <h1 className={styles.title}>Wordle Step</h1>
           <div className={styles.info}>
-            <div className={styles.stage}>x/y</div>
+            <div className={styles.stage}>
+              {currentStage}/{totalStages}
+            </div>
             <button
-              className={styles.length}
+              className={handleGameLengthColor(gameLength)}
               title="Game length"
               onClick={handleNextGameLength}
             >
@@ -33,10 +83,12 @@ export default function Header() {
         </>
       ) : (
         <>
-          <div className={styles.stage}>Stage x/y</div>
+          <div className={styles.stage}>
+            Stage {currentStage}/{totalStages}
+          </div>
           <h1 className={styles.title}>Wordle Step</h1>
           <button
-            className={styles.length}
+            className={handleGameLengthColor(gameLength)}
             title="Game length"
             onClick={handleNextGameLength}
           >
