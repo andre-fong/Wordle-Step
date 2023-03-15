68041e63cd53c1e45f5645f76604df5488c02719
Andre Fong <fongandre123@gmail.com>
2023-03-15 01:26:58 -0400

    chore: Fix meta tags and banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,9 +35,9 @@ export default function Welcome() {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6aaa64" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#6aaa64" />
 
         {/* Facebook Meta Tags */}
         <meta property="og:type" content="website" />
